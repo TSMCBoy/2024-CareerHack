5a4c84f3a912b5c604de656f34e274ba851e1204
coolpeter75 <155715800+coolpeter75@users.noreply.github.com>
2024-01-17 17:38:29 +0800

    Update sample1.py

--- PaLM2/sample1.py
+++ PaLM2/sample1.py
@@ -8,5 +8,5 @@ for response in text_generation_model.predict_streaming(
     prompt="Count to 100",
     max_output_tokens=1000
 ):
-    print(datetime.datetime.now(), "|", response)
+print(datetime.datetime.now(), "|", response)
 print("End: ", datetime.datetime.now())
